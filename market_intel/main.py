b87be8c4b5658ad8975c2cd1095599018e71c568
mbaraka <monsif.baraka@inrs.ca>
2025-08-13 11:19:39 -0400

    f

--- market_intel/main.py
+++
@@ -1,8 +0,0 @@
-import json 
-
-
-with open("mensuel_20240401_20240430.json", 'r', encoding = 'utf8') as f:
-    data = json.load(f)
-project =  data['releases'][10]
-for item in project['contracts'] : 
-    print(item)
--- run_seao.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\mbaraka\Documents\SEAO API"
-call C:\Users\mbaraka\AppData\Local\anaconda3\Scripts\activate.bat det2
-python "C:\Users\mbaraka\Documents\SEAO API\main.py"
